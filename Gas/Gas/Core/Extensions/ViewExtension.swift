b70a8db50450b6399a4c78a9db7e17f04cd4228f
dxmvsh <dimash.live@gmail.com>
2021-04-07 11:41:11 +0600

    feat: add view extension with rounding borders

--- Gas/Gas/Core/Extensions/ViewExtension.swift
+++ Gas/Gas/Core/Extensions/ViewExtension.swift
@@ -5,4 +5,17 @@
 //  Created by Strong on 4/7/21.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func roundCorners(radius: CGFloat) {
+        layer.cornerRadius = radius
+    }
+    
+    func roundCorners(radius: CGFloat, corners: CACornerMask) {
+        layer.maskedCorners = corners
+        layer.cornerRadius = radius
+    }
+    
+}
