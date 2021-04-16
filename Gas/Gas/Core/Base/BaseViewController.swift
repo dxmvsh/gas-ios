7f6ac9e43c17867445edb792d5e5b315b98bb6b3
dxmvsh <dimash.live@gmail.com>
2021-04-16 09:23:28 +0600

    feat(BaseViewController): add default title and subtitle labels

--- Gas/Gas/Core/Base/BaseViewController.swift
+++ Gas/Gas/Core/Base/BaseViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class BaseViewController: UIViewController, UIGestureRecognizerDelegate {
     
+    let titleLabel = LabelFactory.buildTitleLabel()
+        .with(numberOfLines: 0)
+        .with(alignment: .center)
+    
+    let subtitleLabel = LabelFactory.buildSubtitleLabel()
+        .with(numberOfLines: 0)
+        .with(alignment: .center)
+    
     lazy var backButtonTapHandler: (() -> Void)? = { [weak self] in
         self?.navigationController?.popViewController(animated: true)
     }
@@ -57,6 +65,24 @@ class BaseViewController: UIViewController, UIGestureRecognizerDelegate {
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: contactButton)
         supportIcon.snp.makeConstraints { $0.center.equalToSuperview() }
         contactButton.snp.makeConstraints { $0.size.equalTo(36) }
+    }
+    
+    func addTitleAndSubtitleLabels(title: String = "", subtitle: String = "") {
+        [titleLabel, subtitleLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: LayoutGuidance.offset),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: LayoutGuidance.offsetDouble),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -LayoutGuidance.offset),
+            
+            subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: LayoutGuidance.offset),
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: LayoutGuidance.offset),
+            subtitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -LayoutGuidance.offset),
+        ])
     }
     
     @objc
